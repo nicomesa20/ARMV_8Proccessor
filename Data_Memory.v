894f942f322e4993bec3bf2e2285e2e4a9f5d300
Nicolas Mesa <nicomesa2013@gmail.com>
2020-05-25 14:49:30 -0500

    IO data memmory

--- Data_Memory.v
+++ Data_Memory.v
@@ -19,11 +19,14 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module Data_Memory(
+    input i_clk,
     input [63:0] i_add,
     input [63:0] i_dataWr,
+    input [7:0]  i_sw,
     input i_memRd, // Señal de control
     input i_memWr, // Señal de control
-    output [63:0] o_dataRd
+    output [63:0] o_dataRd,
+    output reg [7:0] o_leds
     );
 
     localparam MEM_SIZE = 100;
@@ -34,10 +37,16 @@ module Data_Memory(
     initial begin
 		$readmemb  (inicial, r_insMem);
 	end
+    
+    always @(posedge i_clk) begin
+        if(i_add < MEM_SIZE && i_memWr  && i_add[17:16] == 1)begin
+            o_leds <= i_add[7:0];
+        end        
+    end
 
     // Area de escritura 
     always @(*)
-        if(i_add < MEM_SIZE && i_memWr)begin
+        if(i_add < MEM_SIZE && i_memWr  && i_add[17:16] == 0)begin
             r_insMem [i_add+7] <= i_dataWr[63:56];
             r_insMem [i_add+6] <= i_dataWr[55:48];
             r_insMem [i_add+5] <= i_dataWr[47:40];
@@ -51,7 +60,9 @@ module Data_Memory(
         
     
     // Area de lectura
-    assign o_dataRd = i_add<MEM_SIZE?(i_memRd?{r_insMem[i_add+7],r_insMem[i_add+6],r_insMem[i_add+5],r_insMem[i_add+4],r_insMem[i_add+3],r_insMem[i_add+2],r_insMem[i_add+1],r_insMem[i_add]}:0):0;
+    assign o_dataRd = i_add<MEM_SIZE?(i_memRd && i_add[17:16] == 0?{r_insMem[i_add+7],r_insMem[i_add+6],r_insMem[i_add+5],r_insMem[i_add+4],r_insMem[i_add+3],r_insMem[i_add+2],r_insMem[i_add+1],r_insMem[i_add]}:
+    (i_memRd && i_add[17:16] == 1)?{{56{i_leds[7]}},i_leds}:
+    (i_memRd && i_add[17:16] == 2)?{{56{i_sw[7]}},i_sw}):0;
 
 
 endmodule
